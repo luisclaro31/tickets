1143b073e38ae36ed6f071c3f17388eaa34a03c9
Hector Luis Claro <luisclaro31@outlook.com>
2015-12-22 14:50:45 +0000

    Version Alpha 2.0 -- Correccion Actualizacion Anterior

--- resources/views/home/footer/footer.blade.php
+++ resources/views/home/footer/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="pull-right hidden-xs">
-    <b>Version</b> Beta 1.1
+    <b>Version</b> Alpha 2.0
 </div>
 <strong>Copyright &copy; 2015 construida con Laravel v5.1, Bootstrap v3 and jQuery v2.0.1 Software Creado por <a href="#!">Hector Luis Claro y Hernando de la Hoz</a></strong>
